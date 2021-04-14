a912d6a0b47b19d70f2f01d98257249ab50353d8
weal <chin@cyanbluewave.com>
2021-04-14 16:07:20 +0800

    Updates

---
+++ precache-manifest.f7f0762a72aaa44f8fd0cc23f2985f1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59d2467aaf743d242d0a52209aeb56b",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "48895b92c59b8ea3ad13",
+    "url": "/reactjs/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3b0be4a37ff7231d482b",
+    "url": "/reactjs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48895b92c59b8ea3ad13",
+    "url": "/reactjs/static/js/2.54952d6b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactjs/static/js/2.54952d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0be4a37ff7231d482b",
+    "url": "/reactjs/static/js/main.aaa99be2.chunk.js"
+  },
+  {
+    "revision": "6d645e5f6e75acd8742b",
+    "url": "/reactjs/static/js/runtime-main.3a1bb2d6.js"
+  }
+]);
